102f0fe485b28a512168c74b02a1a299345ad062
Denis Savitskiy <savickijdenis69@gmail.com>
2026-08-07 17:07:05 +0300

    Fix item IT

--- src/test/java/com/dsavitskiy/orderservice/OrderserviceApplicationTests.java
+++ src/test/java/com/dsavitskiy/orderservice/OrderserviceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class OrderserviceApplicationTests {
+class OrderserviceApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
